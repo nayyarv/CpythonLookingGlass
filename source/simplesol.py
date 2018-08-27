47b222aca35a881f2f7b0dde7f73badb17b1f1f5
nayyarv <nayyarv@gmail.com>
2018-08-27 17:21:11 +1000

    Adding timing code too

--- source/simplesol.py
+++ source/simplesol.py
@@ -15,3 +15,14 @@
 [1, 2]
 >>> b
 [1, 2]
+
+
+
+
+
+
+
+
+python3 -m timeit -n 100 -s "N=1000000; a = list(range(N)); b=list(range(N))" "a.extend(b); a = list(range(N)); "
+python3 -m timeit -n 100 -s "N=1000000; a = list(range(N)); b=list(range(N))" "a += b; a = list(range(N)); "
+python3 -m timeit -n 100 -s "N=1000000; a = list(range(N)); b=list(range(N))" "a = a + b; a = list(range(N)); "
---
+++ source/timer.py
@@ -0,0 +1,45 @@
+import timeit
+
+setupStr = """
+a = list(range({N})); b=list(range({N}, 2*{N}))
+"""
+
+statement = {
+    'ext': "a.extend(b)",
+    'iadd': "a += b",
+    'add': "a + b"
+}
+
+
+def main():
+    import collections
+    results = collections.defaultdict(list)
+    powTot = 7
+    for powval in range(1, powTot):
+        results["Npow"].append(powval)
+        listsize, numruns = 10 ** powval, 10 ** (powTot-powval)
+        print(f"N = {listsize}, n = {numruns}")
+        setup = setupStr.format(N=listsize)
+        for method, stmt in statement.items():
+            time = min(timeit.repeat(stmt, setup, repeat=numruns, number=1))
+            print(f"{method}: {time*numruns:.3f}")
+            results[method].append(time)
+
+    import tabulate
+    print(tabulate.tabulate(results, headers='keys', tablefmt="pipe"))
+    from matplotlib import style
+    style.use('ggplot')
+    from matplotlib import pyplot as plt
+    xaxis = results.pop('Npow')
+    for method, res in results.items():
+        plt.semilogy(xaxis, res, label=method)
+    plt.title("Times for various methods")
+    plt.xlabel("Log of listsize")
+    plt.ylabel("Time taken")
+    plt.legend()
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    main()
